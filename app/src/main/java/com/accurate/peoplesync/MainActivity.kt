090b04c68a06dcb6f13352528a3e75cc1e16ef15
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2026-05-13 19:51:07 +0700

    feat(ui): enable edge-to-edge system bar styling
    Apply custom status bar styling using the application primary color.

--- app/src/main/java/com/accurate/peoplesync/MainActivity.kt
+++ app/src/main/java/com/accurate/peoplesync/MainActivity.kt
@@ -2,15 +2,22 @@ package com.accurate.peoplesync
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.toArgb
+import com.accurate.peoplesync.ui.theme.PeopleSyncAppTheme.Color.Companion.PrimaryOrange
 import com.accurate.peoplesync.ui.theme.PeopleSyncTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                scrim = PrimaryOrange.toArgb()
+            )
+        )
 
         setContent {
             PeopleSyncTheme { PeopleSyncNavGraph() }
